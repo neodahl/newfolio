3c787cc6352497141d30ac1f9127b4d74675759a
neodahl <neodahl90@gmail.com>
2019-11-25 17:58:05 +0900

    nav button 작업중

---
+++ asset/js/main.js
@@ -0,0 +1,10 @@
+(function(global, document , $){
+
+/* Nav Menu Button */
+$('.btnNavMenu').on('click', function(){
+  $(this).toggleClass('is_close');
+});
+
+
+
+})(window, window.document, jQuery);
