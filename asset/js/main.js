72c02db85dd1a3adc85b166f5baad98869b7ccc5
neodahl <neodahl90@gmail.com>
2019-12-04 13:47:00 +0900

    모바일 메뉴 모션 완료

--- asset/js/main.js
+++ asset/js/main.js
@@ -8,51 +8,62 @@
 	var lastScrollPosition = 0;
 	var bodyHeight = $('body').outerHeight();
 	var windowHeight = $(window).height();
+	var $btnNavMenu = $('.btnNavMenu');
 	
-	$(window).on('scroll', scrolling);
-	
-	function scrolling(){
+	$(window).on('scroll', windowScrollingCheck);
+
+	function windowScrollingCheck() {
+		if( $btnNavMenu.hasClass('is_close') ) {
+			return
+		} else {
+			windowScrolling();
+		}
+	}
+
+	function windowScrolling(){
 		var bottomBouncingDelta = bodyHeight - (windowHeight + presentTop);
-    presentTop = $(this).scrollTop();
+    presentTop = $(window).scrollTop();
 
 		if (presentTop < headerHeight + 20) {
 			// close to top
 			$('.mainHeader').removeClass('offCanvas');	
-
 		} else if(bottomBouncingDelta < 80) {
 			//prevent ios bottom bouncing 
 			$('.mainHeader').addClass('offCanvas');
-							
 		} else if(presentTop > lastScrollPosition) {
 			//scroll down, header hide
 			$('.mainHeader').addClass('offCanvas');
 			lastScrollPosition = presentTop - 10;
-
 		} else {
 			// else, show header
 			$('.mainHeader').removeClass('offCanvas');
 			lastScrollPosition = presentTop + 10;
 		}
-		
 	}
+
+
+
+
+	
 
 
   // App Nav Show and Hide
   var $mainHeader = $('.mainHeader');
   var $mainNav = $('.mainNav');
-  var $btnNavMenu = $('.btnNavMenu');
+  var $mainNavMenus = $('.mainNav > a');
   var navOpenTL = new TimelineMax();
 
   navOpenTL.pause();
 
   navOpenTL
     .to($btnNavMenu, 0.3, {position: 'fixed', top: 0, left: 0}, 'sync')
-    .set($btnNavMenu, {className: '+=is_closed'}, 'sync')
-    .set($mainHeader, {className: '+=offCanvas'})
-		.to($mainNav, 0, {left: 0}, 'sync');
+    .set($btnNavMenu, {className: '+=is_close'}, 'sync')
+		.to($mainNav, 0, {left: 0}, 'sync')
+		.set($mainHeader, {className: '+=offCanvas'}, 'sync2')
+		.staggerFrom($mainNavMenus, 0.3, {autoAlpha: 0, y: 5}, 0.1, 'sync2');
 		
     $btnNavMenu.on('click', function(){
-      if($(this).hasClass('is_closed')) {
+      if($(this).hasClass('is_close')) {
 				navOpenTL.reverse();
       } else {
 				navOpenTL.restart();
