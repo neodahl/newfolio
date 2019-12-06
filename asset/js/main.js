2414e675c78cc834025577a0fd2147a6b0a90894
neodahl <neodahl90@gmail.com>
2019-12-06 15:15:55 +0900

    CSSRulePlugin 적용중

--- asset/js/main.js
+++ asset/js/main.js
@@ -9,6 +9,7 @@
 	var bodyHeight = $('body').outerHeight();
 	var windowHeight = $(window).height();
 	var $btnNavMenu = $('.btnNavMenu');
+	var $mainHeader = $('.mainHeader');
 	
 	$(window).on('scroll', windowScrollingCheck);
 
@@ -26,41 +27,40 @@
 
 		if (presentTop < headerHeight + 20) {
 			// close to top
-			$('.mainHeader').removeClass('offCanvas');	
+			$mainHeader.removeClass('offCanvas');	
 		} else if(bottomBouncingDelta < 80) {
 			//prevent ios bottom bouncing 
-			$('.mainHeader').addClass('offCanvas');
+			$mainHeader.addClass('offCanvas');
 		} else if(presentTop > lastScrollPosition) {
 			//scroll down, header hide
-			$('.mainHeader').addClass('offCanvas');
+			$mainHeader.addClass('offCanvas');
 			lastScrollPosition = presentTop - 10;
 		} else {
 			// else, show header
-			$('.mainHeader').removeClass('offCanvas');
+			$mainHeader.removeClass('offCanvas');
 			lastScrollPosition = presentTop + 10;
 		}
 	}
 
 
   // App Nav Show and Hide
-  var $mainHeader = $('.mainHeader');
-  var $mainNav = $('.mainNav');
-  var $mainNavMenus = $('.mainNav > a');
+  var mainNav = '.mainNav';
+  var mainNavMenus = '.mainNav > a';
   var navOpenTL = new TimelineMax({paused: true});
   var navCloseTL = new TimelineMax({paused: true});
 
   navOpenTL
     .to($btnNavMenu, 0.3, {position: 'fixed', top: 0, left: 0}, 'sync1')
     .set($btnNavMenu, {className: '+=is_close'}, 'sync1')
-		.to($mainNav, 0.3, {left: 0}, 'sync1')
+		.to(mainNav, 0.3, {left: 0}, 'sync1')
 		.set($mainHeader, {className: '+=offCanvas'}, 'sync2')
-		.staggerFrom($mainNavMenus, 0.3, {autoAlpha: 0, y: 5}, 0.1, 'sync2');
+		.staggerFrom(mainNavMenus, 0.3, {autoAlpha: 0, y: 5}, 0.1, 'sync2');
 	
 	navCloseTL
 		.set($btnNavMenu, {className: '-=is_close'}, 'sync')
 		.set($mainHeader, {className: '-=offCanvas'})
-		.staggerTo($mainNavMenus, 0.3, {autoAlpha: 0, y: 5}, -0.1, 'sync')
-		.to($mainNav, 0.3, {left: '-300px'}, '-=0.1')
+		.staggerTo(mainNavMenus, 0.3, {autoAlpha: 0, y: 5}, -0.1, 'sync')
+		.to(mainNav, 0.3, {left: '-300px'}, '-=0.1')
 		.to($btnNavMenu, 0, {position: 'relative'});
 		
 	$btnNavMenu.on('click', function(){
@@ -84,14 +84,15 @@
 	var topVisualTL = new TimelineMax();
 
 	topVisualTL
-		.to($('h1'), 0, { top: '45%'}, 0.5)
-		.to($('h1 span:first-of-type'), 0, { fontSize: '2.5em'})
-		.from($('h1 span:first-of-type'), 0.6, { autoAlpha: 0}, 'sync1')
-		.from($('h1 span:first-of-type'), 1.2, { y: 50}, 'sync1')
-		.to($('h1'), 0.2, { top: '41%'}, 'sync2')
-		.to($('h1 span:first-of-type'), 1, { fontSize: '1em', ease: Power1.inOut, }, 'sync2')
-		.from($('h1 span:nth-of-type(2)'), 1.5, { autoAlpha: 0, x: -10 })
-		.from($('h1 span:nth-of-type(3)'), 1.5, { autoAlpha: 0, y: 10 }, '-=0.8')
+		.to('h1', 0, {top: '45%'}, 0.5)
+		.to('h1 span:first-of-type', 0, {fontSize: '2.5em'})
+		.from('h1 span:first-of-type', 0.6, {autoAlpha: 0}, 'sync1')
+		.from('h1 span:first-of-type', 1.2, {y: 50}, 'sync1')
+		.to('h1', 0.2, {top: '41%'}, 'sync2')
+		.to('h1 span:first-of-type', 1, {fontSize: '1em', ease: Power1.inOut}, 'sync2')
+		.from('h1 span:nth-of-type(2)', 1.5, {autoAlpha: 0, x: -10})
+		.from('h1 span:nth-of-type(3)', 1.5, {autoAlpha: 0, y: 10}, '-=0.8')
+		// .to(CSSRulePlugin.getRule('h1 span:nth-of-type(3):after'), {cssRule:{color:"red"}})
 		;
 
 })(window, window.document, jQuery);
