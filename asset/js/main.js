12556c02379f6fd1271b0911ef748ff30e72dc97
neodahl_dev_win <neodahl90@gmail.com>
2020-11-17 18:43:48 +0900

    section03 animation completed

--- asset/js/main.js
+++ asset/js/main.js
@@ -49,7 +49,7 @@
 		.from('.section02 .skills dd span', {stagger: 0.1, duration: 0.3, opacity: 0, x: 10}, '-=0.4')
 		;
 
-/* 
+
 	// Section03 Animation
 	var section03_TL = gsap.timeline({
 		scrollTrigger: {
@@ -57,22 +57,20 @@
 			trigger: '.section03',
 			// start: 'top 80%',
 			toggleActions: 'restart none none none',
-			onEnter: function() {
-				console.log('enter');
-			},
-			onLeaveBack: function() {
-				console.log('onLeaveBack');
-			}
-
 		}
 	});
 	section03_TL
 		.from('.section03 h2', {duration: 2, opacity: 0})
-		// .from('.section03 .bg_container .line_1', {duration: 0.5, left: -100, top: -100}, 0)
-		// .from('.section03 .bg_container .line_1', {duration: 0.5, left: 0, top: -1000}, 0)
+		.from('.section03 .layout_center > p', {stagger: 0.2, duration: 0.5, opacity: 0, y:10}, 0.5)
+		.from('.section03 h2 > small:first-of-type:empty', {duration: 0.5, opacity: 0, y:10})
+		.from('.section03 h2 > small:last-of-type:empty', {duration: 0.5, scale: 0}, '-=0.5')
+		.from('.section03 .bg_container .box', {duration: 0.5, opacity: 0, y:10})
+		.from('.section03 .bg_container .line_1', {duration: 1, scale: 0 , x: 1000, y: -1000}, 0.5)
+		.from('.section03 .bg_container .line_2', {duration: 1, scale: 0 , x: -2000, y: 2000}, 0.5)
+		.from('.section03 .bg_container .line_3', {duration: 1, scale: 0 , x: 1000, y: -1000}, 0.5)
 		;
 
- */
+
 
 
 })(window, window.document);
