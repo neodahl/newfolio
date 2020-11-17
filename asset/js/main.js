733699197f75c30d44e6f201f73bb46c445fa22c
neodahl_dev_win <neodahl90@gmail.com>
2020-11-17 12:32:58 +0900

    secction01 animation 추가

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,14 +1,22 @@
-/* Nav Menu Controls */
-(function(global, document , $){
-	'use strict'
+'use strict';
+
+(function(global, document){
+
 
 	var section01TL = gsap.timeline();
 
-	// section01TL
-	// 	.from()
- 
+	section01TL
+		.from('h1 > small', {duration: 0.5, opacity: 0, y: -10}, 0.2)
+		.from('h1 > span', {duration: 0.5, opacity: 0, y: -10}, '-=0.3')
+		.from('h1 + small', {duration: 0.5, opacity: 0, y: -10}, '-=0.3')
+		.from('.section01 .right_msg', {duration: 1, opacity: 0, y: -20})
+		.from('.section01 .img_conatainer', {duration: 2, opacity: 0, y: 50, x:20, scale: 1.1}, '-=1.3')
+		;
 
-})(window, window.document, jQuery);
+
+
+
+})(window, window.document);
 
 
 
